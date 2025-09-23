37f91efc22831f45e2a682e5a036547a6c4022dc
Wade Simpson <wadesimpsongit@gmail.com>
2025-09-23 15:16:15 +0200

    Fixed Padding Issue

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -23,7 +23,7 @@ export const Home = () => {
   }, []);
   
   return (
-    <div className="min-h-screen bg-background text-foreground overflow-x-hidden pr-16">
+    <div className="min-h-screen bg-background text-foreground overflow-x-hidden">
     <div>
       <Navbar/>
       <ThemeToggle />
